3285357bb95cd02353477e62a750847c27e998be
Brian Muñoz <81725406+brianmunioz@users.noreply.github.com>
2022-05-24 00:02:38 -0300

    Update carrito.view.php

--- views/carrito.view.php
+++ views/carrito.view.php
@@ -5,14 +5,22 @@
     <?php if(isset($_SESSION['carrito'])):?>
         <div class="prod">
         <?php foreach($resultado as $prod):?>
+            <div class="item">
+         <h1><?php echo $prod['nombre']  ?></h1>
          
-            <h1><?php echo $prod['nombre']  ?></h1>
             <h3>$<?php echo $prod['precio']  ?> c/u - Cantidad: <?php echo $prod['cantidad']  ?></h3>
-            <?php $totalCarrito += $prod['precio'] *  $prod['cantidad'];?>
+            <div class="elimEdit">
+            <a href="eliminarChar.php?id=<?php echo htmlspecialchars($prod['id'])?> " style="color:red;">eliminar</a>
+            <a href="producto.php?id=<?php echo htmlspecialchars($prod['id'])?> " >Editar</a>
+            </div>
+
+         </div>
+            
+            <?php $totalCarrito += $prod['precio'] *$prod['cantidad'];?>
             
         <?php endforeach; ?>
         <h2>Total: $<?php echo $totalCarrito ?></h2>
-
+        <a href="eliminarChar.php?id=vaciar" style="font-weight: bold;color: red;">Vaciar carrito</a>
         </div>
 
         <a href="checkout-datos.php" class="btn-finalizar">Finalizar compra</a>
